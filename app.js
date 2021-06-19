eafef41c73195b342231dd3ac181479688aff8be
thealitahir <thealitahir431@gmail.com>
2021-06-19 16:41:24 +0500

    Added Cors

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use('/api', routes);
 
 connectDB();
